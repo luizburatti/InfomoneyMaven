1bc80b6483be93382e07c046c8588f467f3f7c55
GabrielRinadlin <gabrielrinaldin@hotmail.com>
2021-03-14 15:44:56 -0300

    First commit

---
+++ src/connection/Dao.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Dao {
+
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/inFomoney?useTimezone=true&serverTimezone=UTC";
+
+	private String user = "root";
+	private String password = "Gabriel@123";
+	
+	
+	private Connection conectar() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public void TesteConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+          System.out.println(e);
+		}
+	}
+	
+
+}
